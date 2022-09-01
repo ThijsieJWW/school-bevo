da2e9daaab6212c7ff58622b156c88b09647f7ab
YourMatto <124151@leerling.delagewaard.nl>
2022-09-01 18:13:32 +0200

    Enemie does Damage now
    and btw if you die the level restart

---
+++ Assets/scripts/Enemie/DamagePlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public int Damage = 1;
+    public float Distance = 1.1f;
+    public LayerMask playerLayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+       RaycastHit2D hit_right;
+       if (hit_right = Physics2D.Raycast(transform.position, transform.right, Distance, playerLayer))
+       {
+            Health health = hit_right.transform.GetComponent<Health>();
+
+            if (health != null)
+            {
+                health.TakeDamage(Damage);
+            }
+       }
+
+        RaycastHit2D hit_left;
+        if (hit_left = Physics2D.Raycast(transform.position, -transform.right, Distance, playerLayer))
+        {
+            Health health = hit_left.transform.GetComponent<Health>();
+
+            if (health != null)
+            {
+                health.TakeDamage(Damage);
+            }
+        }
+    }
+}
--- Assets/scripts/Player/Health.cs
+++ Assets/scripts/Player/Health.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
+    public int Maxhealth = 3;
+    int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = Maxhealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
