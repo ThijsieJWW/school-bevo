5b9d74b54f35aa9b0e4a4a3bc7f497a76eab4371
YourMatto <124151@leerling.delagewaard.nl>
2022-09-01 16:43:46 +0200

    New Sprite

--- Assets/scripts/Player/FlipBody.cs
+++ Assets/scripts/Player/FlipBody.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class FlipBody : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.localScale = new Vector3(4, 4, 1);
-    }
+    public SpriteRenderer SR;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.localScale = new Vector3(4, 4, 1);
+            SR.flipX = false;
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.localScale = new Vector3(-4, 4, 1);
+            SR.flipX = true;   
         }
     }
 }
