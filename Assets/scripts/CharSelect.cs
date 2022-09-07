00ac94b9df18439b9d96fe08fbb57c25ea72c69d
Thijs van Straaten <thijs124010@gmail.com>
2022-09-07 09:53:35 +0200

    CharSelect update

--- Assets/scripts/CharSelect.cs
+++ Assets/scripts/CharSelect.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharSelect : MonoBehaviour
 {
-    public Sprite a;
-    public Sprite b;
-    public Sprite c;
-    public Sprite d;
+    public GameObject[] Characters;
+    public Button NextBtn;
+    public Button PrevBtn;
+
+    private int currChar;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        currChar = 0;
+        NextBtn.onClick.AddListener(Next);
+        PrevBtn.onClick.AddListener(Prev);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateSprites()
     {
+        Transform t;
+        for (int i = 0; i < Characters.Length; i++)
+        {
+            t = Characters[i].GetComponent<Transform>();
+            t.position.Set((currChar * i) * 5,0,0);
+        }
+    }
 
+    void Next()
+    {
+        if (currChar + 1 < Characters.Length) {
+            currChar++;
+        }
+    }
+
+    void Prev()
+    {
+        if (currChar-1 > 0)
+        {
+            currChar--;
+        }
     }
 }
