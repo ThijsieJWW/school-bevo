2ffd2b3af6f8c51e9cf35a219f7a09a114d852de
YourMatto <124151@leerling.delagewaard.nl>
2022-09-07 09:49:27 +0200

    bug next level fixed

--- Assets/scripts/GameManger.cs
+++ Assets/scripts/GameManger.cs
@@ -19,10 +19,7 @@ public class GameManger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (score_script.Current_Score == 20)
-        {
-            SceneManager.LoadScene("Lvl2");
-        }
+        
     }
 
     public void Lvl1_finish()
@@ -31,9 +28,10 @@ public class GameManger : MonoBehaviour
         Won_table.SetActive(true);
     }
 
-    public void NextLVL_button()
+    public void NextLVL_button(string level)
     {
-        SceneManager.LoadScene("Lvl2");
+        Debug.Log("Next level");
+        SceneManager.LoadScene(level);
     }
 
     public void main_Menu()
