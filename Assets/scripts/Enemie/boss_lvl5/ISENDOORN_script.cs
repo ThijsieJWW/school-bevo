5880201940004593eb7b48f32782d9cfbdaa3c8e
YourMatto <124151@leerling.delagewaard.nl>
2022-11-03 17:09:51 +0100

    first prototype of isendoorn trowing

--- Assets/scripts/Enemie/boss_lvl5/ISENDOORN_script.cs
+++ Assets/scripts/Enemie/boss_lvl5/ISENDOORN_script.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class ISENDOORN_script : MonoBehaviour
 {
+    //trow object
+    public GameObject trow_object;
+    public Rigidbody2D trow_object_rigidboy;
+    //attackpoint
+    public Transform attack_point;
+
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Attack", 2);
+        //trow object rigidbody
+        trow_object_rigidboy = trow_object.GetComponent<Rigidbody2D>();
+        Invoke("Attack1", 2);
     }
 
-    void Attack()
+    void Attack1()
     {
-
+        Instantiate(trow_object, attack_point.position, attack_point.rotation);
     }
 }
---
+++ Assets/scripts/trows_isendoorn/Isendoorn_trow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Isendoorn_trow : MonoBehaviour
+{
+    //forces
+    public int minForce;
+    public int maxForce;
+    //refrence to rigidbody
+    public Rigidbody2D rb;
+    //multiplier
+    public float multiplir = 1250;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickrandomnumber(maxForce, minForce);
+    }
+
+    private void pickrandomnumber(int maxint, int minint)
+    {
+        int randomNum = Random.Range(minint+1, maxint+1);
+        rb.AddForce(Time.deltaTime * new Vector3(-randomNum * multiplir, randomNum * multiplir, 0));
+        Debug.Log(randomNum);
+    }
+}
