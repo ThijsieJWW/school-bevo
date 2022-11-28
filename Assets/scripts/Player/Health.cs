daedc44ce913666e289a3c89d1b807cc82f1195f
YourMatto <124151@leerling.delagewaard.nl>
2022-11-28 14:15:53 +0100

    health bar finally

--- Assets/scripts/Player/Health.cs
+++ Assets/scripts/Player/Health.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public int Maxhealth = 3;
     int currentHealth;
-    public Rigidbody2D rb;
+    public int minHp = 0;
+    public int maxHp = 0;
+    public Slider hp_slider;
+    public GameManger gameManger;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = Maxhealth;
+        currentHealth = maxHp;
+        hp_slider.maxValue = maxHp;
+        hp_slider.minValue = minHp;
+    }
+
+    private void Update()
+    {
+        hp_slider.value = currentHealth;
     }
 
     public void TakeDamage(int damage)
