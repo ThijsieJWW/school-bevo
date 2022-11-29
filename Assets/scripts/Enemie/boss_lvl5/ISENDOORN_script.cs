0fa206a89f29bb789c0f708560e7f69a7593af79
YourMatto <124151@leerling.delagewaard.nl>
2022-11-29 22:42:42 +0100

    lvl 5 update
    isendoorn heeft animaties nu !!!!!!

--- Assets/scripts/Enemie/boss_lvl5/ISENDOORN_script.cs
+++ Assets/scripts/Enemie/boss_lvl5/ISENDOORN_script.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Animations;
 
 public class ISENDOORN_script : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class ISENDOORN_script : MonoBehaviour
     public GameManger gamemanger;
     //slider refrence
     public Slider hpslider;
+    //refrences to animations isendoorn
+    public Animator isen_animator;
 
     // Start is called before the first frame update
 
@@ -46,48 +49,66 @@ public class ISENDOORN_script : MonoBehaviour
 
     void Attack1()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack2", 1);
     }
     void Attack2()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack3", 1);
     }
     void Attack3()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack4", 1);
     }
     void Attack4()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack5", 1);
     }
 
     void Attack5()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack6", 1);
     }
     void Attack6()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack7", 1);
     }
     void Attack7()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack8", 1);
     }
     void Attack8()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack9", 1);
     }
 
     void Attack9()
     {
+        //plays animation
+        isen_animator.Play("trow_attack");
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("Attack10", 1);
     }
@@ -95,13 +116,15 @@ public class ISENDOORN_script : MonoBehaviour
     {
         Instantiate(trow_object, attack_point.position, attack_point.rotation);
         Invoke("start_math_problem", 3);
+        //plays animation
+        isen_animator.Play("trow_attack");
     }
 
     void start_math_problem()
     {
         math_problem_panel.SetActive(true);
         math_code.calculte_diff_num();
-        Invoke("end_math_problem", 5);
+        Invoke("end_math_problem", 7.5f);
     }
 
     void end_math_problem()
